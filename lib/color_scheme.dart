08152e7ae8a839f632cb1b4b83c1d4eabb59c88d
satviksriv <srivastavasatvik22@gmail.com>
2021-07-11 05:29:44 +0530

    Added color scheme

---
+++ lib/color_scheme.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+final bgColor = Color(0xff5c4db1);
+final yellowButton = Color(0xffffbf67);
+final pinkButton = Color(0xffdc4f89);
+final veryLightPurpleBg = Color(0xffdfdeff);
