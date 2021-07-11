b685bf6f3d45f48674601e33d4b1303990bfe63d
satviksriv <srivastavasatvik22@gmail.com>
2021-07-11 07:06:51 +0530

    Made the welcome page

---
+++ lib/pages/your_plan.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:home_cleaning_ui/color_scheme.dart';
+
+class YourPlan extends StatefulWidget {
+  const YourPlan({Key? key}) : super(key: key);
+
+  @override
+  _YourPlanState createState() => _YourPlanState();
+}
+
+class _YourPlanState extends State<YourPlan> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
